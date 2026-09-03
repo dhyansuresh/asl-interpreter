40104a7c6414327746db19e48dcc8b2793975e6d
dhyansuresh <98236844+dhyansuresh@users.noreply.github.com>
2026-09-02 20:32:31 -0400

    first set of data passed into model

--- training/train.py
+++ training/train.py
@@ -1,8 +1,11 @@
 import numpy as np
 import json
-from sklearn.model_selection import train_test_split
 
-def train():
+import torch
+from sklearn.model_selection import train_test_split
+from torch import nn
+
+def prepare_data():
     data = np.load('data/landmarks_normalized.npz')
 
     keep = data['labels'] != 'nothing'
@@ -12,15 +15,33 @@ def train():
 
     classes, encoded = np.unique(final_labels, return_inverse=True)
 
-    print(encoded.min(), encoded.max())  # 0, 27
-    print(final_labels[0], encoded[0])  # 'A' and 0
-    print(classes[encoded[0]])  # 'A' — back where you started
-
     json.dump(classes.tolist(), open('data/classes.json', 'w'))
 
     X_train, X_test, y_train, y_test = train_test_split(
         final, encoded, test_size=0.2, stratify=encoded, random_state=42
     )
 
+    return X_train, X_test, y_train, y_test, classes
 
-train()
+
+def train(X_train, X_test, y_train, y_test):
+    X_train = torch.from_numpy(X_train).float()
+    X_test = torch.from_numpy(X_test).float()
+    y_train = torch.from_numpy(y_train).long()
+    y_test = torch.from_numpy(y_test).long()
+
+    model = nn.Sequential(
+        nn.Linear(42, 128),
+        nn.ReLU(),
+        nn.Linear(128, 64),
+        nn.ReLU(),
+        nn.Linear(64, 28),
+    )
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    out = model(X_train[:5])
+    print(out)
+
+X_train, X_test, y_train, y_test, classes = prepare_data()
+train(X_train, X_test, y_train, y_test)
